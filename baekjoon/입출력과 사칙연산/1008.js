196be4ba25fc077b4f4fc833f4b30a80b34227ad
jade <mini4614@gmail.com>
2021-09-11 23:22:06 +0900

    til

---
+++ baekjoon/입출력과 사칙연산/1008.js
@@ -0,0 +1,19 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+let input = []
+
+const handleInput = (line) => {
+  input.push(line)
+}
+
+const handleOutput = () => {
+  const [a, b] = input[0].split(' ').map((i) => parseInt(i))
+  console.log(a / b)
+  process.exit()
+}
+
+rl.on('line', handleInput).on('close', handleOutput)
