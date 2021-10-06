62d945c06b339920c051f7d6dafde5e1fe1381d4
morethanmin <mini4614@gmail.com>
2021-10-06 09:02:22 +0900

    2742

---
+++ baekjoon/for문/2742.js
@@ -0,0 +1,18 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+let input = []
+rl.on('line', (line) => {
+  input.push(line)
+}).on('close', () => {
+  const N = parseInt(input[0])
+  let answer = ''
+  for (let i = N; i >= 1; i--) {
+    answer += i + '\n'
+  }
+  console.log(answer)
+  process.exit()
+})
---
+++ baekjoon/for문/2742.py
@@ -0,0 +1,5 @@
+N = int(input())
+answer = ''
+for i in range(N):
+  answer += f'{N-i} \n'
+print(answer)
