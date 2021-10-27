4d6586c52104146bc3b54eb41ed63eeb67194323
morethanmin <mini4614@gmail.com>
2021-10-27 10:13:11 +0900

    1065

---
+++ baekjoon/함수/1065.py
@@ -0,0 +1,21 @@
+
+def isHanSu(n):
+  strN = str(n)
+  if len(strN) == 1:
+    return True
+  else:
+    chaSet = set()
+    for i in range(len(strN) - 1):
+      chaSet.add(int(strN[i]) - int(strN[i+1]))
+    if len(list(chaSet)) == 1:
+      return True
+    else:
+      return False
+
+N = int(input())
+count = 0
+for i in range(N):
+  if isHanSu(i+1):
+    count = count + 1
+
+print(count)
