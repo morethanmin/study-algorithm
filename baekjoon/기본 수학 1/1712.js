6b744d91db1ba6aaf39daffdbc552d8b1d42521e
morethanmin <mini4614@gmail.com>
2021-11-29 11:29:14 +0900

    1712

--- baekjoon/기본 수학 1/1712.js
+++ baekjoon/기본 수학 1/1712.js
@@ -3,3 +3,19 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
+
+let input = []
+
+rl.on('line', (line) => {
+  input.push(line)
+}).on('close', () => {
+  const [A, B, C] = input[0].split(' ').map((val) => parseInt(val))
+  if (B >= C) console.log(-1)
+  else {
+    console.log(Math.floor(A / (C - B) + 1))
+  }
+  process.exit()
+})
+
+// A + B * n < C * n
+// A / (C - B) < n
