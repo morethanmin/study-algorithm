56d7a6ada395bd39f0a03edeae4eb7fc5156c257
morethanmin <mini4614@gmail.com>
2022-01-05 12:58:05 +0900

    1181

---
+++ baekjoon/단계별로 풀어보기/정렬/1181.js
@@ -0,0 +1,28 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+let inputs = []
+
+rl.on('line', (line) => {
+  inputs.push(line)
+}).on('close', () => {
+  const N = inputs.shift()
+  const arrays = Array.from(new Set(inputs))
+  const sortedArrays = arrays.sort((word, nextWord) => {
+
+    if (word.length === nextWord.length) {
+
+      return (word) > (nextWord) ? 1 : -1
+    } else {
+      return word.length - nextWord.length
+    }
+
+
+  })
+
+  console.log(sortedArrays.join('\n'))
+  process.exit()
+})
