261e94d0a5a765f159f53fcffa8361f31d880c2f
morethanmin <mini4614@gmail.com>
2022-01-06 13:27:57 +0900

    18870

--- baekjoon/단계별로 풀어보기/정렬/18870.js
+++ baekjoon/단계별로 풀어보기/정렬/18870.js
@@ -1,13 +1,20 @@
-const readline = require('readline')
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+const fs = require('fs')
 
-let inputs = []
+const inputs = fs.readFileSync('/dev/stdin').toString().split('\n')
+const nums = inputs[1].split(' ').map((i) => Number(i))
 
-rl.on('line', (line) => {
-  inputs.push(line)
-}).on('close', () => {
-  process.exit()
-})
+//중복되는 값 set 자료구조를 이용해 제거 후 정렬
+const numsSet = [...new Set(nums)].sort((a, b) => a - b)
+
+let dic = {}
+let answer = ''
+
+//numsSet에 있는 값을 dic에 넣어둠.
+numsSet.forEach((value, i) => { dic[value] = i })
+
+//해당 값에 맞는 값을 가져온다.
+for (let i = 0; i < nums.length; i++) {
+  answer += dic[nums[i]] + ' ';
+}
+
+console.log(answer);
