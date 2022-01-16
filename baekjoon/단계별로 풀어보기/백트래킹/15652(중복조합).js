202de3ad71bbbb0ec48d5a8362f5600e01068075
morethanmin <mini4614@gmail.com>
2022-01-16 13:56:10 +0900

    15652

--- baekjoon/단계별로 풀어보기/백트래킹/15652(중복조합).js
+++ baekjoon/단계별로 풀어보기/백트래킹/15652(중복조합).js
@@ -1,17 +1,34 @@
 const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 })
 
-let inputs =[]
+let inputs = []
 
 const getMultisetCombination = (arr, selectNumber) => {
-  const result = []
+  const results = []
 
-  if(selectNumber === 1) return arr.map((el) => [el])
-  arr.forEach((value,index,origin) => {
-    const rest = origin,
-    
-  });
+  if (selectNumber === 1) return arr.map((el) => [el])
+  arr.forEach((value, index, origin) => {
+    const rest = origin.slice(index)
+    const combinations = getMultisetCombination(rest, selectNumber - 1)
+    const attached = combinations.map((el) => [value, ...el])
+    results.push(...attached)
+  })
+  return results
 }
+
+rl.on('line', (line) => {
+  inputs.push(line)
+}).on('close', () => {
+  const [N, M] = inputs[0].split(' ').map((i) => +i)
+  const array = [...Array(N + 1).keys()]
+  array.shift()
+  console.log(
+    getMultisetCombination(array, M)
+      .map((val) => val.join(' '))
+      .join(`\n`)
+  )
+  process.exit()
+})
