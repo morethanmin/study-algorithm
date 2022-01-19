3062c5f1cfbc158da0edbdf052f71ab3ab2f8869
Octoping <53991994+Octoping925@users.noreply.github.com>
2022-01-19 19:46:02 +0900

    Create 7482.js

---
+++ problem/7482.js
@@ -0,0 +1,10 @@
+// ㄹㅇ 진짜 찐 수학문제
+// 저는 코드 딱 2줄로 풀었습니다 (파이썬)
+// fighting
+// https://www.acmicpc.net/problem/7482
+
+ const readline = require('readline')
+ const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+ })
