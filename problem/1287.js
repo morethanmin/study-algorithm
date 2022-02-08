5d78fb65d451abb8f9cb1569f8558824212babaa
morethanmin <mini4614@gmail.com>
2022-02-08 13:35:51 +0900

    n퀸 복습

--- problem/1287.js
+++ problem/1287.js
@@ -1,3 +1,5 @@
+// 아직 실패
+
 const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,5 +12,6 @@ rl.on('line',(line)=>{
   inputs.push(line)
 }).on('close',()=>{
   const 식 = inputs[0]
+  if(식.includes('()')) return console.log('ROCK');
   console.log(eval(식));
 })
--- 대표 알고리즘/공부용/index.js
+++ 대표 알고리즘/공부용/index.js
@@ -1,39 +1,41 @@
 /**
- * my ps play ground
+ * 
  */
 
-// n queen n * n 체스판에서 n개의 퀸을 놓을 수 있는 경우의 수를 구해보자.
+// n queen n * n 체스판에서 n개의 퀸을 놓을 수 있는 경우의 수
 
 const nQueenSolution = (N) => {
-  let count = 0
   const board = new Array(N + 1).fill(0)
+  let count = 0
 
-  const isPromising = (y) => {
-    for (let i = 1; i < y; i++) {
+  function isPromising(n) {
+    for (let i = 1; i < n; i++) {
       if (
-        board[i] === board[y] ||
-        Math.abs(board[y] - board[i]) === y - i
-      ) {
+        // 일직선상인경우
+        board[i] === board[n] ||
+        // 대각선상인경우        
+        Math.abs(board[i] - board[n]) === Math.abs(n - i)
+      )
         return false
-      }
     }
     return true
   }
 
-  const nQueen = (y) => {
-    if (isPromising(y)) {
-      if (y === N) {
-        count++
-      }
-      else {
-        for (let x = 1; x <= N; x++) {
-          board[y + 1] = x
-          nQueen(y + 1)
+  function dfs(n) {
+    if (isPromising(n)) {
+      if (N === n) {
+        count = count + 1
+        return
+      } else {
+        for (let i = 1; i <= N; i++) {
+          board[n + 1] = i
+          dfs(n + 1)
         }
       }
     }
   }
-  nQueen(0)
+  dfs(0)
+
   return count
 }
 console.log('nQueen(8)');
