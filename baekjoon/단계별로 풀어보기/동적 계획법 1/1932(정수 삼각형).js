688047a09c225a256d29e4236469a0b2d29e03db
morethanmin <mini4614@gmail.com>
2022-02-15 10:57:31 +0900

    1932

--- baekjoon/단계별로 풀어보기/동적 계획법 1/1932(정수 삼각형).js
+++ baekjoon/단계별로 풀어보기/동적 계획법 1/1932(정수 삼각형).js
@@ -3,19 +3,14 @@ const readFileSyncAddress = '/dev/stdin'
 const input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n')
 const n = +input.shift()
 const triangle = input.map((row) => row.split(' ').map((el) => +el))
-console.log(triangle);
 
 function solution(N, arr) {
-  const memo = [...new Array(N)].map((el, idx) => new Array(idx+1).fill(0))
-  memo[0][0] = arr[0][0]
   for (let i = 1; i < arr.length; i++) {
-    for (let j = 0; j < i.length; j++) {
-      memo[i][j] = Math.max(arr[i + 1][j], arr[i + 1][j + 1]) + memo[i][j]
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = Math.max((arr[i - 1][j - 1] ? arr[i - 1][j - 1] : 0), (arr[i - 1][j] ? arr[i - 1][j] : 0)) + arr[i][j]
     }
   }
-  return memo
+  return Math.max(...arr[N - 1])
 }
 
 console.log(solution(n, triangle));
-
-//둘중 하나 중에서 최대 값을 가져온다.
