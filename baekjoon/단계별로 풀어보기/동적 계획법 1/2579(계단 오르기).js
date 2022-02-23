89961a071da1efadabb1fd77619f5bf74538921c
morethanmin <mini4614@gmail.com>
2022-02-23 11:56:26 +0900

    2156

--- baekjoon/단계별로 풀어보기/동적 계획법 1/2579(계단 오르기).js
+++ baekjoon/단계별로 풀어보기/동적 계획법 1/2579(계단 오르기).js
@@ -12,12 +12,22 @@ function solution(scores, N) {
   //첫째칸가는경우 최대값
   memo[0] = scores[0]
   //둘째칸가는경우 최대값, 한칸 or 두칸
-  memo[1] = Math.max(scores[0] + scores[1], scores[1])
+  memo[1] = Math.max(
+    scores[0] + scores[1],
+    scores[1]
+  )
   //셋째칸가는경우 최대값, 한칸 두칸 or 두칸 한칸
-  memo[2] = Math.max(scores[0] + scores[2], scores[1] + scores[2])
+  memo[2] = Math.max(
+    scores[0] + scores[2],
+    scores[1] + scores[2]
+  )
+
   for (let i = 3; i < N; i++) {
     //i째칸가는경우 최대값, i칸 i-1칸 i-3최대값 or i칸 i-2최대값
-    memo[i] = Math.max(scores[i] + scores[i - 1] + memo[i - 3], scores[i] + memo[i - 2])
+    memo[i] = Math.max(
+      scores[i] + scores[i - 1] + memo[i - 3],
+      scores[i] + memo[i - 2]
+    )
   }
   return memo[N - 1]
 }
